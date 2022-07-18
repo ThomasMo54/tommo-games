6348f8bbc717fbb05f568316aa57946f0ad22c4c
TomMo <tmoretti@motompro.com>
2022-07-18 21:29:46 +0200

    feat: Created copy code button in waiting room panel

--- client/src/main/java/com/motompro/tommogames/client/window/panel/WaitingRoomPanel.java
+++ client/src/main/java/com/motompro/tommogames/client/window/panel/WaitingRoomPanel.java
@@ -4,6 +4,7 @@ import com.motompro.tommogames.common.Game;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.datatransfer.StringSelection;
 
 public class WaitingRoomPanel extends JPanel {
 
@@ -20,13 +21,22 @@ public class WaitingRoomPanel extends JPanel {
         this.setLayout(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
         // Code
-        JPanel codePanel = new JPanel(new GridLayout(1, 2));
+        JPanel codePanel = new JPanel(new GridLayout(1, 3));
         JLabel codeTitleLabel = new JLabel("Code : ");
         codeTitleLabel.setFont(new Font("verdana", Font.PLAIN, 16));
         codePanel.add(codeTitleLabel);
-        JLabel codeLabel = new JLabel(code);
+        JTextField codeLabel = new JTextField(code);
+        codeLabel.setEditable(false);
+        codeLabel.setBackground(null);
+        codeLabel.setBorder(null);
         codeLabel.setFont(new Font("verdana", Font.BOLD, 16));
         codePanel.add(codeLabel);
+        JButton copyButton = new JButton("Copier");
+        copyButton.addActionListener(e -> {
+            StringSelection selection = new StringSelection(code);
+            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection, selection);
+        });
+        codePanel.add(copyButton);
         constraints.anchor = GridBagConstraints.FIRST_LINE_START;
         constraints.insets = new Insets(20, 20, 20, 0);
         constraints.weightx = 0;
