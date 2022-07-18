1645508d81a12efe5800eaf51825da232ebf8a20
TomMo <tmoretti@motompro.com>
2022-07-18 20:22:05 +0200

    feat: Created WaitingRoom panel

--- client/src/main/java/com/motompro/tommogames/client/WaitingRoom.java
+++ client/src/main/java/com/motompro/tommogames/client/WaitingRoom.java
@@ -1,15 +1,19 @@
 package com.motompro.tommogames.client;
 
 import com.motompro.tcplib.client.ServerListener;
+import com.motompro.tommogames.client.window.panel.WaitingRoomPanel;
 import com.motompro.tommogames.common.Game;
 
 public class WaitingRoom implements ServerListener {
 
     private final Game game;
+    private final WaitingRoomPanel panel;
 
     public WaitingRoom(Game game) {
         this.game = game;
+        this.panel = new WaitingRoomPanel(game);
         TomMoGames.getInstance().getClient().addServerListener(this);
+        TomMoGames.getInstance().getMainWindow().showPanel(panel);
     }
 
     @Override
---
+++ client/src/main/java/com/motompro/tommogames/client/window/panel/WaitingRoomPanel.java
@@ -0,0 +1,19 @@
+package com.motompro.tommogames.client.window.panel;
+
+import com.motompro.tommogames.common.Game;
+
+import javax.swing.*;
+
+public class WaitingRoomPanel extends JPanel {
+
+    private final Game game;
+
+    public WaitingRoomPanel(Game game) {
+        this.game = game;
+        init();
+    }
+
+    private void init() {
+        
+    }
+}
